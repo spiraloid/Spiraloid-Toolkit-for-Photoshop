da940456023d55c4efb8d6003c6397b0bfe33c6c
Spiraloid <bay.raitt@gmail.com>
2023-07-09 12:18:13 -0700

    minor fix

--- br_incremental_save.jsx
+++ br_incremental_save.jsx
@@ -33,97 +33,69 @@ if (app.documents.length !== 0) {
     'captain', 'duke', 'emperor', 'king', 'prince', 'sultan', 'pharaoh', 'tsar', 'kaiser', 'shogun', 'warlord', 'chieftain', 'overlord', 
     'lord', 'master', 'guru', 'sage', 'prophet', 'wizard', 'elder', 'patriarch', 'matriarch', 'priest', 'bishop', 'cardinal', 'pope'];
   
-  // Generate uniqueFilename at the beginning as you currently do
-var moniker = monikerList[Math.floor(Math.random() * monikerList.length)];
-var adjective = adjectiveList[Math.floor(Math.random() * adjectiveList.length)];
-var noun = nounList[Math.floor(Math.random() * nounList.length)];
-var uniqueFilename = moniker + "." + adjective + "." + noun;
+    // Generate uniqueFilename at the beginning as you currently do
+    var moniker = monikerList[Math.floor(Math.random() * monikerList.length)];
+    var adjective = adjectiveList[Math.floor(Math.random() * adjectiveList.length)];
+    var noun = nounList[Math.floor(Math.random() * nounList.length)];
+    var uniqueFilename = moniker + "." + adjective + "." + noun;
 
-// Get active document
-var file = app.activeDocument;
-var extension = "." + file.name.split(".").pop();
-
-// If the file has never been saved
-if (!file.saved) {
-  neverSaved = true;
-  var assetName = uniqueFilename;
-  var highestVersion = 0;  // Start with version 0
-} else {
-  // If the file has been saved
-  docPath = file.path + "/";
-
-  // Use regex to match assetName and version
-  var match = file.name.match(/(.*?)_v\.(\d+)/);
-
-  if (match) {
-    // If the file name contains a version
-    var assetName = match[1];
-    var currentVersion = parseInt(match[2], 10);
-  } else {
-    // If the file name doesn't contain a version, assume it's the first version
-    var assetName = file.name.split(extension)[0];
-    // Set the currentVersion to -1 if it's the first version
-    var currentVersion = -1;  
-  }
-
-  // Get all files in the folder
-  var files = Folder(quicksavePath).getFiles();
-  var highestVersion = currentVersion;
-
-  // Loop through the files to find the highest version
-  for (var i = 0; i < files.length; i++) {
-    if (files[i] instanceof File) {
-      var fileMatch = files[i].name.match(/(.*?)_v\.(\d+)/);
-      // Check if the file begins with the assetName and contains a version
-      if (fileMatch && fileMatch[1] === assetName) {
-        var version = parseInt(fileMatch[2], 10);
-        if (version > highestVersion) {
-          highestVersion = version;
+    // ... Your adjectiveList, nounList, and monikerList ...
+  
+    // If the file has been saved
+    if (doc.saved) {
+      docPath = doc.path + "/";
+      // Use regex to match assetName and version
+      var match = doc.name.match(/(.*)(_v\.)(\d+)/);
+      var assetName = match[1];
+      var versionNumber = match[3];
+      var currentVersion = parseInt(versionNumber);
+    } else {
+      // Generate uniqueFilename for never saved files
+      var moniker = monikerList[Math.floor(Math.random() * monikerList.length)];
+      var adjective = adjectiveList[Math.floor(Math.random() * adjectiveList.length)];
+      var noun = nounList[Math.floor(Math.random() * nounList.length)];
+      var assetName = moniker + "." + adjective + "." + noun;
+      var currentVersion = -1;
+      docPath = quicksavePath;
+    }
+  
+    // Get all files in the folder
+    var files = Folder(docPath).getFiles();
+    var highestVersion = currentVersion;
+  
+    // Loop through the files to find the highest version
+    for (var i = 0; i < files.length; i++) {
+      if (files[i] instanceof File) {
+        var fileMatch = files[i].name.match(/(.*)(_v\.)(\d+)/);
+        // Check if the file begins with the assetName and contains a version
+        if (fileMatch && fileMatch[1] === assetName) {
+          var version = parseInt(fileMatch[3]);
+          if (version > highestVersion) {
+            highestVersion = version;
+          }
         }
       }
     }
-  }
-
-  // Increment the highest version for the next save
-  highestVersion++;
-
-}
-
-// Zero padding function
-function zeroPad(num, places) {
-  var zero = places - num.toString().length + 1;
-  return Array(+(zero > 0 && zero)).join("0") + num;
-}
-
-  // // Log variables
-  // logMessage("Variables:");
-  // logMessage("  - assetName: " + assetName);
-  // logMessage("  - currentVersion: " + currentVersion);
-  // logMessage("  - highestVersion: " + highestVersion);
-  // // var incrementedVersion = zeroPad((parseInt(currentVersion, 10) + 1), 3);
-  // // logMessage("  - incrementedVersion: " + incrementedVersion);
-  // logMessage("  - newFilename: " + newFilename);
-
-  // logFile.close(); // Don't forget to close the log file after writing
-
-
-  // Export as PNG
-  if (neverSaved) { 
-    var pngExportPath = quicksavePath + assetName + ".png";
-    var newFilename = quicksavePath + assetName + "_v." + zeroPad(highestVersion, 3) + ".psd";
-  }
-  else {
-    var pngExportPath = docPath + assetName  + ".png";
+  
+    // Increment the highest version for the next save
+    highestVersion++;
+  
+    // Zero padding function
+    function zeroPad(num, places) {
+      var zero = places - num.toString().length + 1;
+      return Array(+(zero > 0 && zero)).join("0") + num;
+    }
+  
+    // Prepare the new filename
     var newFilename = docPath + assetName + "_v." + zeroPad(highestVersion, 3) + ".psd";
+    var pngExportPath = docPath + assetName  + ".png";
+  
+    // Export as PNG
+    var pngFile = new File(pngExportPath);
+    doc.exportDocument(pngFile, ExportType.SAVEFORWEB, undefined);
+  
+    // Save the file
+    var saveFile = new File(newFilename);
+    doc.saveAs(saveFile);
   }
   
-  var pngFile = new File(pngExportPath);
-  doc.exportDocument(pngFile, ExportType.SAVEFORWEB, undefined);
-
-
-  // Save the file
-  var saveFile = new File(newFilename);
-  doc.saveAs(saveFile);
-  
-
-}
