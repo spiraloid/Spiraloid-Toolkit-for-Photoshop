84b7a876239ca48407814f1c328946f039008a61
Spiraloid <bay.raitt@gmail.com>
2023-07-08 21:01:41 -0700

    added more name strings to make the naming more unique

--- br_incremental_save.jsx
+++ br_incremental_save.jsx
@@ -1,10 +1,37 @@
 if (app.documents.length !== 0) {
   var doc = app.activeDocument;
   var quicksavePath = "X:/Prod/Sketchbook/Photoshop/Quicksaves Psd/";
-
-  var monikerList = ['grokable', 'gnarly', 'clean', 'groggy', 'sleepy', 'deadly', 'dedicated', 'industrious', 'secret', 'miss', 'mister', 'completely', 'one', 'the', 'stinky', 'wicked', 'towering', 'friendly', 'quick', 'fast', 'rough', 'thin', 'fat', 'scrawny', 'slow'];
-  var adjectiveList = ['scrappy', 'contemptuous', 'bequeathed', 'sparkling', 'sapient', 'suave', 'sexy', 'villainous', 'heroic', 'olympian', 'pirate', 'kungfu', 'badass', 'mojo', 'dry', 'crumbling', 'wet', 'slimy', 'brittle', 'solid', 'spooky', 'funny', 'adorable', 'scary', 'bitchin'];
-  var nounList = ['pickle', 'curmudgeon', 'monstrosity', 'froggy', 'fighter', 'alien', 'totem', 'trophy', 'peach', 'spitsculpt', 'experiment', 'test', 'table', 'charriot', 'relic', 'tome', 'pal', 'friend', 'flatmate', 'contender', 'mobile', 'beastie', 'muffin', 'squirrel', 'ninja', 'plushy', 'block', 'blob', 'gak', 'thing', 'bust', 'sculpt', 'model', 'sketch'];
+  
+  var monikerList = [
+    'grokable', 'gnarly', 'clean', 'groggy', 'sleepy', 'deadly', 'dedicated', 'industrious', 'secret', 'miss', 'mister', 
+    'completely', 'one', 'the', 'stinky', 'wicked', 'towering', 'friendly', 'quick', 'fast', 'rough', 'thin', 'fat', 'scrawny', 'slow',
+    'funny', 'ecstatic', 'surprised', 'dejected', 'curious', 'relaxed', 'anxious', 'innocent', 'smart', 'silly', 'honest', 'loyal', 
+    'brave', 'ambitious', 'generous', 'calm', 'lucky', 'reluctant', 'meticulous', 'persistent', 'foolish', 'confident', 'wild', 
+    'courageous', 'jolly', 'joyful', 'determined', 'dynamic', 'enchanting', 'enthusiastic', 'exotic', 'gutsy', 'happy', 'inquisitive', 
+    'jovial', 'kind', 'lively', 'nimble', 'obedient', 'plucky', 'quirky', 'ravishing', 'sincere', 'thoughtful', 'unusual', 'vivacious', 'wacky', 'zealous'];
+  
+  var adjectiveList = [
+    'scrappy', 'contemptuous', 'bequeathed', 'sparkling', 'sapient', 'suave', 'sexy', 'villainous', 'heroic', 'olympian', 'pirate', 
+    'kungfu', 'badass', 'mojo', 'dry', 'crumbling', 'wet', 'slimy', 'brittle', 'solid', 'spooky', 'funny', 'adorable', 'scary', 'bitchin',
+    'ancient', 'modern', 'foreign', 'domestic', 'urban', 'rural', 'sassy', 'silly', 'spoiled', 'rich', 'poor', 'smart', 'dumb', 'lazy', 
+    'busy', 'careful', 'careless', 'risk-taking', 'shy', 'outgoing', 'serious', 'light-hearted', 'evil', 'kind', 'creative', 'destructive', 
+    'healthy', 'sick', 'dreamy', 'sensitive', 'lazy', 'energetic', 'short', 'tall', 'medium', 'average', 'gloomy', 'bright', 'dark', 
+    'light', 'cool', 'warm', 'drab', 'colorful', 'fluffy', 'smooth', 'rough', 'odd', 'typical', 'rare', 'common', 'straight', 'curved', 
+    'circular', 'square', 'triangular', 'American', 'French', 'Italian', 'British', 'Mexican', 'Spanish', 'Chinese', 'Japanese', 'Canadian'];
+  
+  var nounList = [
+    'pickle', 'curmudgeon', 'monstrosity', 'froggy', 'fighter', 'alien', 'totem', 'trophy', 'peach', 'spitsculpt', 'experiment', 'test', 
+    'table', 'charriot', 'relic', 'tome', 'pal', 'friend', 'flatmate', 'contender', 'mobile', 'beastie', 'muffin', 'squirrel', 'ninja', 
+    'plushy', 'block', 'blob', 'gak', 'thing', 'bust', 'sculpt', 'model', 'sketch', 'robot', 'dragon', 'goblin', 'zombie', 'elf', 'dwarf',
+    'vampire', 'werewolf', 'fairy', 'witch', 'wizard', 'mummy', 'ghoul', 'centaur', 'mermaid', 'unicorn', 'pegasus', 'griffin', 'kraken',
+    'orc', 'troll', 'golem', 'pixie', 'sprite', 'nymph', 'gorgon', 'sphinx', 'phoenix', 'chimera', 'minotaur', 'sasquatch', 'yeti', 
+    'alien', 'cyborg', 'android', 'ninja', 'samurai', 'pirate', 'knight', 'cowboy', 'bandit', 'mage', 'paladin', 'monk', 'assassin', 
+    'druid', 'bard', 'ranger', 'cleric', 'warlock', 'sorcerer', 'alchemist', 'jester', 'templar', 'berserker', 'huntsman', 'blacksmith',
+    'barbarian', 'gladiator', 'musketeer', 'necromancer', 'shaman', 'witch-doctor', 'hermit', 'explorer', 'viking', 'nomad', 'chieftain',
+    'captain', 'duke', 'emperor', 'king', 'prince', 'sultan', 'pharaoh', 'tsar', 'kaiser', 'shogun', 'warlord', 'chieftain', 'overlord', 
+    'lord', 'master', 'guru', 'sage', 'prophet', 'wizard', 'elder', 'patriarch', 'matriarch', 'priest', 'bishop', 'cardinal', 'pope'];
+  
+  
   var moniker = monikerList[Math.floor(Math.random() * monikerList.length)];
   var adjective = adjectiveList[Math.floor(Math.random() * adjectiveList.length)];
   var noun = nounList[Math.floor(Math.random() * nounList.length)];
